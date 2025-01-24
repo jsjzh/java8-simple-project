b818aa4de77c0b942fbfdf0614a22204858b97a8
jinzhehao <jinzhehao@souche.com>
2025-01-24 15:15:45 +0800

    添加 dev tools

--- src/main/java/com/jsjzh/java8/simple/project/controller/HelloController.java
+++ src/main/java/com/jsjzh/java8/simple/project/controller/HelloController.java
@@ -1,12 +1,15 @@
 package com.jsjzh.java8.simple.project.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
+@RequestMapping("/hello")
 public class HelloController {
 
-  @RequestMapping("/hello")
+  @RequestMapping("/world")
+  @ResponseBody
   public String hello () {
     return "Hello world";
   }
