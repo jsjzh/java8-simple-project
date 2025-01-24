a2ed99812728147e131faeafb4c5d094820f7ac8
jinzhehao <jinzhehao@souche.com>
2025-01-24 14:44:50 +0800

    project init

---
+++ src/main/java/com/jsjzh/java8/simple/project/Java8SimpleProjectApplication.java
@@ -0,0 +1,13 @@
+package com.jsjzh.java8.simple.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8SimpleProjectApplication {
+
+  public static void main (String[] args) {
+    SpringApplication.run(Java8SimpleProjectApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/jsjzh/java8/simple/project/Java8SimpleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsjzh.java8.simple.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8SimpleProjectApplicationTests {
+
+  @Test
+  void contextLoads () {
+  }
+
+}
